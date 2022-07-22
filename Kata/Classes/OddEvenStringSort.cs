f4811eda6effc5e3eede1785213f455b3c8aadd7
Auvsexvc <auvsexvc@gmail.com>
2022-07-22 23:34:19 +0200

    Odd-Even String Sort

---
+++ Kata/Classes/OddEvenStringSort.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class OddEvenStringSortTests
+    {
+        private static object[] testCases = new object[]
+        {
+            new object[] {"CodeWars", "CdWr oeas"},
+            new object[] {"YCOLUE'VREER", "YOU'RE CLEVER"},
+        };
+
+        [Test, TestCaseSource("testCases")]
+        public void Test(string s, string expected) => Assert.AreEqual(expected, OddEvenStringSort.SortMyString(s));
+    }
+
+    internal class OddEvenStringSort
+    {
+        public static string SortMyString(string s) =>
+            string
+                .Concat(Enumerable
+                    .Range(0, s.Length)
+                    .GroupBy(i => i % 2 == 0)
+                    .SelectMany(g => g
+                        .Select(e => s[e])
+                        .Append(' ')))
+                .TrimEnd();
+    }
+}
