7cca1e3946450b5b9c1935bda519e387468dac21
Auvsexvc <auvsexvc@gmail.com>
2022-07-23 19:57:35 +0200

    Don't give me five!

---
+++ Kata/Classes/DontGiveMeFive.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(8, DontGiveMeFiveKata.DontGiveMeFive(1, 9));
+            Assert.AreEqual(12, DontGiveMeFiveKata.DontGiveMeFive(4, 17));
+        }
+    }
+
+    internal class DontGiveMeFiveKata
+    {
+        public static int DontGiveMeFive(int start, int end) =>
+            Enumerable.Range(start, end - (start - 1)).Count(x => !x.ToString().Contains('5'));
+    }
+}
