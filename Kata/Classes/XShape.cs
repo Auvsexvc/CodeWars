95fdee3097942eb8c529e037190aacf6858fec8a
Auvsexvc <auvsexvc@gmail.com>
2022-07-24 10:56:41 +0200

    ASCII Fun #1: X- Shape

---
+++ Kata/Classes/XShape.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class XShapeTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.That(XShape.X(3), Is.EqualTo("■□■\n□■□\n■□■"));
+            Assert.That(XShape.X(5), Is.EqualTo("■□□□■\n□■□■□\n□□■□□\n□■□■□\n■□□□■"));
+        }
+    }
+
+    internal class XShape
+    {
+        public static string X(int n) =>
+            string.Join(
+                '\n',
+                Enumerable
+                    .Range(0, n)
+                    .Select(row => string.Concat(Enumerable
+                        .Range(0, n)
+                        .Select(col => col == row || col == Math.Abs(row - (n - 1))
+                            ? '■'
+                            : '□'))));
+    }
+}
