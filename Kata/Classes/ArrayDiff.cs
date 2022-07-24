b4c9d938edfca78e7be86414f57cf88eb555015c
Auvsexvc <auvsexvc@gmail.com>
2022-07-24 19:09:51 +0200

    Array.diff

---
+++ Kata/Classes/ArrayDiff.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Classes
+{
+    internal class ArrayDiffKata
+    {
+        public static int[] ArrayDiff(int[] a, int[] b)
+        {
+            return a.Where(x=> !b.Contains(x)).ToArray();
+        }
+    }
+
+    [TestFixture]
+    public class SolutionTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(new int[] { 2 }, ArrayDiffKata.ArrayDiff(new int[] { 1, 2 }, new int[] { 1 }));
+            Assert.AreEqual(new int[] { 2, 2 }, ArrayDiffKata.ArrayDiff(new int[] { 1, 2, 2 }, new int[] { 1 }));
+            Assert.AreEqual(new int[] { 1 }, ArrayDiffKata.ArrayDiff(new int[] { 1, 2, 2 }, new int[] { 2 }));
+            Assert.AreEqual(new int[] { 1, 2, 2 }, ArrayDiffKata.ArrayDiff(new int[] { 1, 2, 2 }, new int[] { }));
+            Assert.AreEqual(new int[] { }, ArrayDiffKata.ArrayDiff(new int[] { }, new int[] { 1, 2 }));
+            Assert.AreEqual(new int[] { 3 }, ArrayDiffKata.ArrayDiff(new int[] { 1, 2, 3 }, new int[] { 1, 2 }));
+        }
+    }
+}
