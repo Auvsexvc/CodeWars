114879a355bd5710a4f094a159d9f186ab28a78c
Auvsexvc <auvsexvc@gmail.com>
2022-07-24 19:27:11 +0200

    Sum of Digits / Digital Root

---
+++ Kata/Classes/SumOfDigits.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Classes
+{
+    internal class SumOfDigits
+    {
+        public int DigitalRoot(long n)
+        {
+            int i = n.ToString().Sum(c => int.Parse($"{c}"));
+            return i.ToString().Length == 1 ? i : DigitalRoot(i);
+        }
+    }
+
+    [TestFixture]
+    public class NumberTest
+    {
+        private SumOfDigits _num;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _num = new SumOfDigits();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _num = null;
+        }
+
+        [Test]
+        public void Tests()
+        {
+            Assert.AreEqual(7, _num.DigitalRoot(16));
+            Assert.AreEqual(6, _num.DigitalRoot(456));
+        }
+    }
+}
