299f8799e75f89d38ef46b74fd72f463c9a7caf4
Auvsexvc <auvsexvc@gmail.com>
2022-07-25 11:20:02 +0200

    Adding Big Numbers

---
+++ Kata/Classes/AddingBigNumbers.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Numerics;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class AddingBigNumbersTest
+    {
+        [Test]
+        public void ASimpleKataTest()
+        {
+            Assert.AreEqual("110", AddingBigNumbers.Add("91", "19"));
+            Assert.AreEqual("1111111111", AddingBigNumbers.Add("123456789", "987654322"));
+            Assert.AreEqual("1000000000", AddingBigNumbers.Add("999999999", "1"));
+        }
+    }
+
+    internal class AddingBigNumbers
+    {
+        public static string Add(string a, string b) =>
+            BigInteger.Add(BigInteger.Parse(a), BigInteger.Parse(b)).ToString();
+    }
+}
