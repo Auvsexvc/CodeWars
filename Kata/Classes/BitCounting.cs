ffadc798e85ba3f745f87a81dbb1ad4e1fb54a5a
Auvsexvc <auvsexvc@gmail.com>
2022-07-28 20:59:59 +0200

    Bit Counting

---
+++ Kata/Classes/BitCounting.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class BitCountingTest
+    {
+        [Test]
+        public void CountBits()
+        {
+            Assert.AreEqual(0, BitCounting.CountBits(0));
+            Assert.AreEqual(1, BitCounting.CountBits(4));
+            Assert.AreEqual(3, BitCounting.CountBits(7));
+            Assert.AreEqual(2, BitCounting.CountBits(9));
+            Assert.AreEqual(2, BitCounting.CountBits(10));
+        }
+    }
+
+    internal class BitCounting
+    {
+        public static int CountBits(int n)
+        {
+            //BitArray b = new BitArray(new int[] { n });
+            //bool[] bits = new bool[b.Count];
+            //b.CopyTo(bits, 0);
+            //byte[] bitValues = bits.Select(bit=>(byte)(bit ? 1 : 0)).ToArray();
+            //return bitValues.Count(b=>b==1);
+
+            return Convert.ToString(n, 2).Count(b => b == '1');
+        }
+    }
+}
