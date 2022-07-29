5cddbc8a6ed78d2ae865a26d28736988fba3c493
Auvsexvc <auvsexvc@gmail.com>
2022-07-29 12:07:58 +0200

    Small enough?

---
+++ Kata/Classes/SmallEnoughKata.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class SmallEnoughKataTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(true, SmallEnoughKata.SmallEnough(new int[] { 66, 101 }, 200));
+            Assert.AreEqual(false, SmallEnoughKata.SmallEnough(new int[] { 78, 117, 110, 99, 104, 117, 107, 115 }, 100));
+            Assert.AreEqual(true, SmallEnoughKata.SmallEnough(new int[] { 101, 45, 75, 105, 99, 107 }, 107));
+            Assert.AreEqual(true, SmallEnoughKata.SmallEnough(new int[] { 80, 117, 115, 104, 45, 85, 112, 115 }, 120));
+        }
+    }
+
+    internal class SmallEnoughKata
+    {
+        public static bool SmallEnough(int[] a, int limit) => a.All(x => x <= limit);
+    }
+}
