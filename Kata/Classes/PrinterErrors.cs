76507c466557dce9df9aba07feb9b8386d4d35ab
Auvsexvc <auvsexvc@gmail.com>
2022-07-29 11:47:04 +0200

    Printer Errors

---
+++ Kata/Classes/PrinterErrors.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public static class PrinterTests
+    {
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Testing PrinterError");
+            string s = "aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz";
+            Assert.AreEqual("3/56", PrinterErrors.PrinterError(s));
+        }
+    }
+
+    internal class PrinterErrors
+    {
+        public static string PrinterError(String s) =>
+            string.Join("/", s.Count(c => c > 'm'), s.Length);
+    }
+}
