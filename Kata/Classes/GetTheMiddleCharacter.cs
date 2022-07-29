dac296a11a0475c57106ea0cbd5a01c0232a9bc7
Auvsexvc <auvsexvc@gmail.com>
2022-07-29 10:11:59 +0200

    Get the middle character

---
+++ Kata/Classes/GetTheMiddleCharacter.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class GetTheMiddleCharacterTest
+    {
+        [Test]
+        public void GenericTests()
+        {
+            Assert.AreEqual("es", GetTheMiddleCharacter.GetMiddle("test"));
+            Assert.AreEqual("t", GetTheMiddleCharacter.GetMiddle("testing"));
+            Assert.AreEqual("dd", GetTheMiddleCharacter.GetMiddle("middle"));
+            Assert.AreEqual("A", GetTheMiddleCharacter.GetMiddle("A"));
+        }
+    }
+
+    internal class GetTheMiddleCharacter
+    {
+        public static string GetMiddle(string s) =>
+            s.Length % 2 == 0 ? s.Substring((s.Length / 2) - 1, 2) : s.Substring(s.Length / 2, 1);
+    }
+}
