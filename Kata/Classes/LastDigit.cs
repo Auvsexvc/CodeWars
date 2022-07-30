ed645a20e95c26f51bc9d4165f54d787af9afe99
Auvsexvc <auvsexvc@gmail.com>
2022-07-30 18:05:34 +0200

    Last Digit

--- Kata/Classes/LastDigit.cs
+++ Kata/Classes/LastDigit.cs
@@ -2,10 +2,10 @@
 using System.Linq;
 using System.Numerics;
 
-namespace Kata
+namespace Kata.Classes
 {
     [TestFixture]
-    public class SolutionTest
+    public class LastDigitTest
     {
         [Test]
         public void ExampleTests()
