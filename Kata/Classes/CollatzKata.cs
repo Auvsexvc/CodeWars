58e8cc3f5ea95f370877c3e5736ea5a8f399b96b
Auvsexvc <auvsexvc@gmail.com>
2022-07-30 18:23:29 +0200

    Collatz

---
+++ Kata/Classes/CollatzKata.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class CollatzTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("3->10->5->16->8->4->2->1", CollatzKata.Collatz(3));
+        }
+    }
+
+    internal static class CollatzKata
+    {
+        public static string Collatz(int n)
+        {
+            List<string> returnList = new() { n.ToString() };
+            while (n > 1)
+            {
+                n = (n % 2) switch
+                {
+                    0 => n / 2,
+                    _ => (3 * n) + 1,
+                };
+                returnList.Add($"{n}");
+            }
+
+            return String.Join("->", returnList);
+        }
+    }
+}
