503c97c9621d4242965a2f9f9aa580dd159d6c8f
Auvsexvc <auvsexvc@gmail.com>
2022-08-01 15:01:18 +0200

    Beginner Series #4 Cockroach

---
+++ Kata/Classes/Kmph2Cmps.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Kata.Classes
+{
+    public class CockTest1
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual(30, Kmph2Cmps.CockroachSpeed(1.08));
+            Assert.AreEqual(30, Kmph2Cmps.CockroachSpeed(1.09));
+            Assert.AreEqual(0, Kmph2Cmps.CockroachSpeed(0));
+        }
+    }
+
+    internal class Kmph2Cmps
+    {
+        public static int CockroachSpeed(double x) => (int)Math.Floor(x / 0.036);
+    }
+}
