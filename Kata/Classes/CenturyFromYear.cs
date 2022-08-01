e8fce2b3b7e4409710b46fda0a34bb5970e3c19f
Auvsexvc <auvsexvc@gmail.com>
2022-08-01 10:59:26 +0200

    Century From Year

---
+++ Kata/Classes/CenturyFromYear.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class CenturyFromYearTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(18, CenturyFromYear.СenturyFromYear(1705));
+            Assert.AreEqual(19, CenturyFromYear.СenturyFromYear(1900));
+            Assert.AreEqual(17, CenturyFromYear.СenturyFromYear(1601));
+            Assert.AreEqual(20, CenturyFromYear.СenturyFromYear(2000));
+        }
+    }
+
+    internal static class CenturyFromYear
+    {
+        public static int СenturyFromYear(int year) =>
+            year % 100 == 0 ? year / 100 : (year / 100) + 1;
+    }
+}
