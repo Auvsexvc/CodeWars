a3e5c74ef88b6311a3f933aec923046c88dc2bef
Auvsexvc <auvsexvc@gmail.com>
2022-08-08 17:15:32 +0200

    SongPlayList

---
+++ Kata/Classes/SongPlaylist.cs
@@ -0,0 +1,36 @@
+﻿namespace Kata.Classes
+{
+    internal class SongPlaylist
+    {
+        public class Song
+        {
+            private string name;
+
+            public Song NextSong { get; set; }
+
+            public Song(string name)
+            {
+                this.name = name;
+            }
+
+            public bool IsRepeatingPlaylist()
+            {
+                var song = this;
+                var next = NextSong;
+
+                while (song != null && next != null)
+                {
+                    if (ReferenceEquals(song, next))
+                    {
+                        return true;
+                    }
+
+                    song = song.NextSong;
+                    next = next.NextSong?.NextSong;
+                }
+
+                return false;
+            }
+        }
+    }
+}
