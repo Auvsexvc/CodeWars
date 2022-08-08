8eb201b1a9ad9cd6c4e1a186a0bf3aef7e0a7412
Auvsexvc <auvsexvc@gmail.com>
2022-08-08 17:27:25 +0200

    https://www.codewars.com/kata/553e8b195b853c6db4000048/solutions/csharp

---
+++ Kata/Classes/AllUnique.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    public class AllUnique
+    {
+        public static bool HasUniqueChars(string str) =>
+            str.Distinct().Count() == str.Length;
+    }
+
+    public class UniqueTestClass
+    {
+        [Test]
+        public void TestHasUniqueChars1()
+        {
+            Assert.IsTrue(AllUnique.HasUniqueChars("abcdef"));
+        }
+
+        [Test]
+        public void TestHasUniqueChars2()
+        {
+            Assert.False(AllUnique.HasUniqueChars("++-"));
+        }
+
+        [Test]
+        public void TestHasUniqueChars3()
+        {
+            Assert.False(AllUnique.HasUniqueChars("  nAa"));
+        }
+
+        [Test]
+        public void TestHasUniqueChars4()
+        {
+            Assert.False(AllUnique.HasUniqueChars("aba"));
+        }
+    }
+}
