0a4c664133773b73c16c42dadf870550a34ae857
Auvsexvc <auvsexvc@gmail.com>
2022-08-08 17:15:17 +0200

    BinarySearchTree

---
+++ Kata/Classes/BinarySearchTree.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Classes
+{
+    public class Node
+    {
+        public int Value { get; set; }
+
+        public Node Left { get; set; }
+
+        public Node Right { get; set; }
+
+        public Node(int value, Node left, Node right)
+        {
+            Value = value;
+            Left = left;
+            Right = right;
+        }
+    }
+
+    public class BinarySearchTree
+    {
+        public static bool Contains(Node root, int value)
+        {
+            if (root != null)
+            {
+                if (value == root.Value) return true;
+
+                if (value < root.Value)
+                    return Contains(root.Left, value);
+                else
+                    return Contains(root.Right, value);
+            }
+
+            return false;
+        }
+    }
+}
