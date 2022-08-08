29ff2c082cafc1c23e84252b228fd4d5e0f8a9ee
Auvsexvc <auvsexvc@gmail.com>
2022-08-08 17:23:55 +0200

    https://www.codewars.com/kata/56f695399400f5d9ef000af5/solutions/csharp

---
+++ Kata/Classes/IsThisMyTail.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace Kata.Classes
+{
+    public class IsThisMyTail
+    {
+        public static bool CorrectTail(string body, string tail)
+        {
+            string sub = body[^tail.Length..];
+
+            return sub == tail;
+        }
+
+        [TestFixture]
+        public class Sample_Test
+        {
+            private static IEnumerable<TestCaseData> TestCases
+            {
+                get
+                {
+                    yield return new TestCaseData("Fox", "x").Returns(true);
+                    yield return new TestCaseData("Rhino", "o").Returns(true);
+                    yield return new TestCaseData("Meerkat", "t").Returns(true);
+                    yield return new TestCaseData("Emu", "t").Returns(false);
+                    yield return new TestCaseData("Badger", "s").Returns(false);
+                    yield return new TestCaseData("Giraffe", "d").Returns(false);
+                }
+            }
+
+            [Test, TestCaseSource("TestCases")]
+            public bool Test(string body, string tail) => IsThisMyTail.CorrectTail(body, tail);
+        }
+    }
+}
