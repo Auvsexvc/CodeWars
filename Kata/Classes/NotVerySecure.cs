9bf6356bc0dc702458c1414620bb371f549a297a
Auvsexvc <auvsexvc@gmail.com>
2022-08-09 13:33:36 +0200

    https://www.codewars.com/kata/526dbd6c8c0eb53254000110/solutions/csharp

---
+++ Kata/Classes/NotVerySecure.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    public class NotVerySecure
+    {
+        public static bool Alphanumeric(string str) =>
+            str.Length > 0 && str.Count(char.IsLetterOrDigit) == str.Length;
+    }
+
+    [TestFixture]
+    public class NotVerySecureTests
+    {
+        private static IEnumerable<TestCaseData> testCases
+        {
+            get
+            {
+                yield return new TestCaseData("Mazinkaiser").Returns(true);
+                yield return new TestCaseData("hello world_").Returns(false);
+                yield return new TestCaseData("PassW0rd").Returns(true);
+                yield return new TestCaseData("     ").Returns(false);
+            }
+        }
+
+        [Test, TestCaseSource("testCases")]
+        public bool Test(string str) => NotVerySecure.Alphanumeric(str);
+    }
+}
