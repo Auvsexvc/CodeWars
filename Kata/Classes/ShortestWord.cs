f659f028d2cf3c5d1bcf25df066aab7f2a5a6ac0
Auvsexvc <auvsexvc@gmail.com>
2022-08-09 11:34:33 +0200

    https://www.codewars.com/kata/57cebe1dc6fdc20c57000ac9/solutions/csharp

---
+++ Kata/Classes/ShortestWord.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class ShortestWordTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(2, ShortestWord.FindShort("Let's travel abroad shall we"));
+            Assert.AreEqual(3, ShortestWord.FindShort("bitcoin take over the world maybe who knows perhaps"));
+            Assert.AreEqual(3, ShortestWord.FindShort("turns out random test cases are easier than writing out basic ones"));
+        }
+    }
+
+    internal class ShortestWord
+    {
+        public static int FindShort(string s) => Regex.Matches(s, @"([\w\']+)").GroupBy(m => m.Value.Length).Min(g => g.Key);
+    }
+}
