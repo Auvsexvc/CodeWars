8fc74e6a60ae3f48cd10d268cdc9a12739b3267c
Auvsexvc <auvsexvc@gmail.com>
2022-08-09 10:31:26 +0200

    https://www.codewars.com/kata/5168bb5dfe9a00b126000018/solutions/csharp

---
+++ Kata/Classes/ReversedStrings.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    public class ReversedStrings
+    {
+        public static string Solution(string str) => string.Concat(str.Reverse());
+    }
+
+    [TestFixture]
+    public class ReversedStringsTests
+    {
+        [Test]
+        public void World()
+        {
+            Assert.AreEqual("dlrow", ReversedStrings.Solution("world"));
+        }
+    }
+}
