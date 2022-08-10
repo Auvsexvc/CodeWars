db6fad40e32d4139324b67c2c7afd7b3d56cedea
Auvsexvc <auvsexvc@gmail.com>
2022-08-10 15:39:57 +0200

    https://www.codewars.com/kata/56a3f08aa9a6cc9b75000023/solutions/csharp

---
+++ Kata/Classes/SimpleValidationUsernameRegex.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace Kata.Classes
+{
+    public class SimpleValidationUsernameRegex
+    {
+        public static bool ValidateUsr(string username)
+        {
+            // use regex here
+            return Regex.Match(username, @"^([a-z_\d]){4,16}$").Success;
+        }
+    }
+
+    public class SimpleValidationUsernameRegexTests
+    {
+        [Test]
+        [TestCase("asddsa", ExpectedResult = true)]
+        [TestCase("a", ExpectedResult = false)]
+        [TestCase("Hass", ExpectedResult = false)]
+        [TestCase("Hasd_12ssssssssssssssasasasasasssasassss", ExpectedResult = false)]
+        [TestCase("", ExpectedResult = false)]
+        [TestCase("____", ExpectedResult = true)]
+        [TestCase("012", ExpectedResult = false)]
+        [TestCase("p1pp1", ExpectedResult = true)]
+        [TestCase("asd43 34", ExpectedResult = false)]
+        [TestCase("asd43_34", ExpectedResult = true)]
+        public static bool FixedTest(string username)
+        {
+            return SimpleValidationUsernameRegex.ValidateUsr(username);
+        }
+    }
+}
