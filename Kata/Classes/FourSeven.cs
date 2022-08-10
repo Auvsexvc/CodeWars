e5fdc2af0c965eca1abe28481e7232cc7f492c2b
Auvsexvc <auvsexvc@gmail.com>
2022-08-10 14:25:24 +0200

    https://www.codewars.com/kata/5ff50f64c0afc50008861bf0/solutions/csharp

---
+++ Kata/Classes/FourSeven.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace Kata.Classes
+{
+    public class FourSevenKata
+    {
+        public static int FourSeven(int num)
+        {
+            while (num == 4)
+            {
+                return 7;
+            }
+            while (num == 7)
+            {
+                return 4;
+            }
+            return 0;
+        }
+    }
+
+    [TestFixture]
+    public class FourSevenTest
+    {
+        [Test]
+        public void CorrectTests()
+        {
+            Assert.AreEqual(7, FourSevenKata.FourSeven(4));
+            Assert.AreEqual(4, FourSevenKata.FourSeven(7));
+        }
+
+        [Test]
+        public void WrongTests()
+        {
+            Assert.AreEqual(0, FourSevenKata.FourSeven(100));
+            Assert.AreEqual(0, FourSevenKata.FourSeven(-17));
+        }
+    }
+}
