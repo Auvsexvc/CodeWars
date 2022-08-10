603a147fbdbe0c9303fd186346f51ce07084b346
Auvsexvc <auvsexvc@gmail.com>
2022-08-10 14:29:33 +0200

    https://www.codewars.com/kata/57a0e5c372292dd76d000d7e/solutions/csharp

---
+++ Kata/Classes/StringRepeat.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    public class StringRepeat
+    {
+        public static string RepeatStr(int n, string s) => string.Concat(Enumerable.Repeat(s, n));
+    }
+
+    [TestFixture]
+    public class StringRepeatTest
+    {
+        [Test(Description = "Fixed Tests")]
+        public void FixedTests()
+        {
+            Assert.AreEqual("***", StringRepeat.RepeatStr(3, "*"));
+            Assert.AreEqual("#####", StringRepeat.RepeatStr(5, "#"));
+            Assert.AreEqual("ha ha ", StringRepeat.RepeatStr(2, "ha "));
+        }
+    }
+}
