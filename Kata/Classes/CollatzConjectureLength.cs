a03e996852dd370f1a12d5af96053611a13114be
Auvsexvc <auvsexvc@gmail.com>
2022-08-11 09:38:10 +0200

    https://www.codewars.com/kata/54fb963d3fe32351f2000102/solutions/csharp

---
+++ Kata/Classes/CollatzConjectureLength.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class CollatzConjectureLengthTest
+    {
+        [Test]
+        public void SampleTest()
+        {
+            Assert.AreEqual(8, CollatzConjectureLength.Collatz(20));
+            Assert.AreEqual(18, CollatzConjectureLength.Collatz(15));
+        }
+    }
+
+    internal class CollatzConjectureLength
+    {
+        public static int Collatz(int n)
+        {
+            int ret = 1;
+            while (n > 1)
+            {
+                n = (n % 2) switch
+                {
+                    0 => n / 2,
+                    _ => (n * 3) + 1,
+                };
+                ret++;
+            }
+
+            return ret;
+        }
+    }
+}
