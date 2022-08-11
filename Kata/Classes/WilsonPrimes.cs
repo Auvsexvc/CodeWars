8d98eb85d293cad321c57a6c2c16dd14ac6e6f7c
Auvsexvc <auvsexvc@gmail.com>
2022-08-11 10:10:37 +0200

    https://www.codewars.com/kata/55dc4520094bbaf50e0000cb/solutions/csharp

---
+++ Kata/Classes/WilsonPrimes.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Numerics;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        [TestCase(5, ExpectedResult = true)]
+        [TestCase(9, ExpectedResult = false)]
+        [TestCase(6, ExpectedResult = false)]
+        public static bool FixedTest(int p)
+        {
+            return WilsonPrimes.AmIWilson(p);
+        }
+    }
+
+    internal class WilsonPrimes
+    {
+        public static bool AmIWilson(int p) => (FactorialFn(p - 1) + 1) % (p * p) == 0;
+
+        private static BigInteger FactorialFn(int n) => (n == 0) ? 1 : n * FactorialFn(n - 1);
+    }
+}
