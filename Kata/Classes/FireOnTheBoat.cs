44c8f19eb4ba02e621699cb82b8cfbc7420952f2
Auvsexvc <auvsexvc@gmail.com>
2022-08-12 10:58:38 +0200

    https://www.codewars.com/kata/57e8fba2f11c647abc000944/solutions/csharp

---
+++ Kata/Classes/FireOnTheBoat.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata.Classes
+{
+    public class FireOnTheBoat
+    {
+        public static string FireFight(string s)
+        {
+            return s.Replace("Fire","~~");
+        }
+    }
+
+    [TestFixture]
+    public class KataTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("Boat Rudder Mast Boat Hull Water ~~ Boat Deck Hull ~~ Propeller Deck ~~ Deck Boat Mast", FireOnTheBoat.FireFight("Boat Rudder Mast Boat Hull Water Fire Boat Deck Hull Fire Propeller Deck Fire Deck Boat Mast"));
+            Assert.AreEqual("Mast Deck Engine Water ~~", FireOnTheBoat.FireFight("Mast Deck Engine Water Fire"));
+            Assert.AreEqual("~~ Deck Engine Sail Deck ~~ ~~ ~~ Rudder ~~ Boat ~~ ~~ Captain", FireOnTheBoat.FireFight("Fire Deck Engine Sail Deck Fire Fire Fire Rudder Fire Boat Fire Fire Captain"));
+        }
+    }
+}
