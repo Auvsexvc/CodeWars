8bde6b8037512dc61348f46f8dbe71ad9b9e4e04
Auvsexvc <auvsexvc@gmail.com>
2022-08-13 11:37:24 +0200

    https://www.codewars.com/kata/544a54fd18b8e06d240005c0/solutions/csharp

---
+++ Kata/Classes/SmallestValueOfAnArray.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class FindSmallest
+    {
+        [Test]
+        public void Test_54321_value()
+        {
+            Assert.AreEqual(1, SmallestValueOfAnArray.FindSmallest(new int[] { 5, 4, 3, 2, 1 }, "value"));
+        }
+
+        [Test]
+        public void Test_54321_index()
+        {
+            Assert.AreEqual(4, SmallestValueOfAnArray.FindSmallest(new int[] { 5, 4, 3, 2, 1 }, "index"));
+        }
+    }
+
+    internal class SmallestValueOfAnArray
+    {
+        public static int FindSmallest(int[] numbers, string toReturn) => toReturn switch
+        {
+            "index" => Enumerable.Range(0, numbers.Length).First(i => numbers[i] == numbers.Min()),
+            _ => numbers.Min(),
+        };
+    }
+}
