929c29e35d15ddc650000759708a60b73442f541
Auvsexvc <auvsexvc@gmail.com>
2022-08-13 12:07:02 +0200

    https://www.codewars.com/kata/57d1f36705c186d018000813/solutions/csharp

---
+++ Kata/Classes/HellsKitchen.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    public class HellsKitchen
+    {
+        public static string Gordon(string a)
+        {
+            return string
+                .Concat(a.Select(c => char.ToLower(c) == 'a'
+                    ? '@'
+                    : new char[] { 'e', 'i', 'o', 'u' }.Contains(char.ToLower(c))
+                        ? '*'
+                        : c))
+                .ToUpper()
+                .Replace(" ", "!!!! ") + "!!!!";
+        }
+    }
+
+    [TestFixture]
+    public class HellsKitchenTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("WH@T!!!! F*CK!!!! D@MN!!!! C@K*!!!!", HellsKitchen.Gordon("What feck damn cake"));
+            Assert.AreEqual("@R*!!!! Y**!!!! ST*!!!! P*D!!!!", HellsKitchen.Gordon("are you stu pid"));
+            Assert.AreEqual("*!!!! @M!!!! @!!!! CH*F!!!!", HellsKitchen.Gordon("i am a chef"));
+        }
+    }
+}
