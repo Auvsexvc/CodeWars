63535144cc6585ab73f3fd9a6f2de8f5cf36a32e
Auvsexvc <auvsexvc@gmail.com>
2022-08-13 12:12:28 +0200

    https://www.codewars.com/kata/5656b6906de340bd1b0000ac/solutions/csharp

---
+++ Kata/Classes/TwoToOne.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public static class TwoToOneTests
+    {
+        [Test]
+        public static void test1()
+        {
+            Assert.AreEqual("aehrsty", TwoToOne.Longest("aretheyhere", "yestheyarehere"));
+            Assert.AreEqual("abcdefghilnoprstu", TwoToOne.Longest("loopingisfunbutdangerous", "lessdangerousthancoding"));
+            Assert.AreEqual("acefghilmnoprstuy", TwoToOne.Longest("inmanylanguages", "theresapairoffunctions"));
+        }
+    }
+
+    public class TwoToOne
+    {
+        public static string Longest(string s1, string s2)
+        {
+            return string.Concat(s1.Union(s2).OrderBy(c => c));
+        }
+    }
+}
