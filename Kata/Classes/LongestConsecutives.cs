dc4fd144ca3b77504ac4a4c03f7708371090a2ca
Auvsexvc <auvsexvc@gmail.com>
2022-08-15 20:08:40 +0200

    https://www.codewars.com/kata/56a5d994ac971f1ac500003e/solutions/csharp

---
+++ Kata/Classes/LongestConsecutives.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public static class LongestConsecutivesTests
+    {
+        [Test]
+        public static void test1()
+        {
+            Console.WriteLine("Basic Tests");
+            testing(LongestConsecutives.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas", "theta", "abigail" }, 2), "abigailtheta");
+            testing(LongestConsecutives.LongestConsec(new String[] { "ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh" }, 1), "oocccffuucccjjjkkkjyyyeehh");
+            testing(LongestConsecutives.LongestConsec(new String[] { }, 3), "");
+            testing(LongestConsecutives.LongestConsec(new String[] { "itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck" }, 2), "wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck");
+            testing(LongestConsecutives.LongestConsec(new String[] { "wlwsasphmxx", "owiaxujylentrklctozmymu", "wpgozvxxiu" }, 2), "wlwsasphmxxowiaxujylentrklctozmymu");
+            testing(LongestConsecutives.LongestConsec(new String[] { "zone", "abigail", "theta", "form", "libe", "zas" }, -2), "");
+            testing(LongestConsecutives.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 3), "ixoyx3452zzzzzzzzzzzz");
+            testing(LongestConsecutives.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 15), "");
+            testing(LongestConsecutives.LongestConsec(new String[] { "it", "wkppv", "ixoyx", "3452", "zzzzzzzzzzzz" }, 0), "");
+        }
+
+        private static void testing(string actual, string expected)
+        {
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    internal class LongestConsecutives
+    {
+        public static string LongestConsec(string[] strarr, int k)
+        {
+            return (strarr.Length > 0 && k <= strarr.Length)
+                ? Enumerable
+                    .Range(0, strarr.Length)
+                    .Select(i => string.Concat(strarr
+                        .Skip(i)
+                        .Take(k)))
+                    .OrderByDescending(k => k.Length)
+                    .FirstOrDefault()
+                : "";
+        }
+    }
+}
