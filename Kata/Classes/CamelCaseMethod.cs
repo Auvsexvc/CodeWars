6b4e47270cfe8c2863c61982a826d89712989310
Auvsexvc <auvsexvc@gmail.com>
2022-08-15 20:55:56 +0200

    https://www.codewars.com/kata/587731fda577b3d1b0001196/solutions/csharp

---
+++ Kata/Classes/CamelCaseMethod.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System.Globalization;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class CamelCaseMethodTest
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual("TestCase", "test case".CamelCase());
+            Assert.AreEqual("CamelCaseMethod", "camel case method".CamelCase());
+            Assert.AreEqual("SayHello", "say hello".CamelCase());
+            Assert.AreEqual("CamelCaseWord", " camel case word".CamelCase());
+            Assert.AreEqual("", "".CamelCase());
+        }
+    }
+
+    internal static class CamelCaseMethod
+    {
+        public static string CamelCase(this string str)
+        {
+            return str.Length > 0
+                ? string.Concat(str
+                    .TrimStart(' ')
+                    .TrimEnd(' ')
+                    .Split(' ')
+                    .Select(s => CultureInfo.CurrentCulture.TextInfo.ToTitleCase(s)))
+                : "";
+        }
+    }
+}
