a30acb0505a11f477f3a6f655cd3d3b34c07bfb5
Auvsexvc <auvsexvc@gmail.com>
2022-08-15 20:26:11 +0200

    https://www.codewars.com/kata/550554fd08b86f84fe000a58/solutions/csharp

---
+++ Kata/Classes/WhichAreIn.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class WhichAreInTests
+    {
+        [Test]
+        public void Test1()
+        {
+            string[] a1 = new string[] { "arp", "live", "strong" };
+            string[] a2 = new string[] { "lively", "alive", "harp", "sharp", "armstrong" };
+            string[] r = new string[] { "arp", "live", "strong" };
+            Assert.AreEqual(r, WhichAreIn.inArray(a1, a2));
+        }
+    }
+
+    internal class WhichAreIn
+    {
+        public static string[] inArray(string[] array1, string[] array2)
+        {
+            var ret =  array1
+                .Where(x => array2.Where(y => y.Contains(x)).Any())
+                .OrderBy(x => x)
+                .ToArray();
+
+            return ret;
+        }
+    }
+}
