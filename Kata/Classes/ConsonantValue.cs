d3805ef910223909f52a1e85bef62142e5d2e472
Auvsexvc <auvsexvc@gmail.com>
2022-08-17 15:34:57 +0200

    https://www.codewars.com/kata/59c633e7dcc4053512000073/solutions/csharp

---
+++ Kata/Classes/ConsonantValue.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class ConsonantValueTests
+    {
+        [TestCase("zodiac", 26)]
+        [TestCase("chruschtschov", 80)]
+        [TestCase("khrushchev", 38)]
+        [TestCase("strength", 57)]
+        [TestCase("catchphrase", 73)]
+        [TestCase("twelfthstreet", 103)]
+        [TestCase("mischtschenkoana", 80)]
+        public void BasicTests(string input, int expected)
+        {
+            Assert.That(ConsonantValue.Solve(input), Is.EqualTo(expected));
+        }
+    }
+
+    public class ConsonantValue
+    {
+        public static int Solve(string s)
+        {
+            return s
+                .Split(new char[] { 'a', 'e', 'i', 'o', 'u' })
+                .GroupBy(x => x
+                    .Select(c => (int)char.ToUpper(c) - 64)
+                    .Sum())
+                .Max(x => x.Key);
+        }
+    }
+}
