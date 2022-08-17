f08e6ed1a62c2ca7da750aa45ecce908700bccd2
Auvsexvc <auvsexvc@gmail.com>
2022-08-17 14:11:29 +0200

    https://www.codewars.com/kata/578aa45ee9fd15ff4600090d/solutions/csharp

---
+++ Kata/Classes/SortTheOdd.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    public class SortTheOdd
+    {
+        public static int[] SortArray(int[] array)
+        {
+            Queue<int> queue = new(array.Where(x => x % 2 != 0).OrderBy(x => x));
+
+            return array.Select(i => i % 2 == 0 ? i : queue.Dequeue()).ToArray();
+        }
+    }
+
+    [TestFixture]
+    public class SortTheOddTests
+    {
+        [Test]
+        public void BasicTests()
+        {
+            Assert.AreEqual(new int[] { 1, 3, 2, 8, 5, 4 }, SortTheOdd.SortArray(new int[] { 5, 3, 2, 8, 1, 4 }));
+            Assert.AreEqual(new int[] { 1, 3, 5, 8, 0 }, SortTheOdd.SortArray(new int[] { 5, 3, 1, 8, 0 }));
+            Assert.AreEqual(new int[] { }, SortTheOdd.SortArray(new int[] { }));
+        }
+    }
+}
