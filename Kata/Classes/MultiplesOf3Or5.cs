860fc72e75ebf7b47ca9cc87896d8130dca22c17
Auvsexvc <auvsexvc@gmail.com>
2022-08-17 13:40:43 +0200

    https://www.codewars.com/kata/514b92a657cdc65150000006/solutions/csharp

---
+++ Kata/Classes/MultiplesOf3Or5.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    public class MultiplesOf3Or5
+    {
+        public static int Solution(int value)
+        {
+            return ListMultiplesOf3Or5(value).Sum();
+        }
+
+        public static IEnumerable<int> ListMultiplesOf3Or5(int value)
+        {
+            for (int i = 0; i < value; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    yield return i;
+                }
+            }
+        }
+    }
+
+    [TestFixture]
+    public class MultiplesOf3Or5Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(23, MultiplesOf3Or5.Solution(10));
+        }
+    }
+}
