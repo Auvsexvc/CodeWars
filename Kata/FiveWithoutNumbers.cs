2350e4e816925f8c58e43791fb52af0e67f05d54
Auvsexvc <auvsexvc@gmail.com>
2022-08-19 22:53:25 +0200

    https://www.codewars.com/kata/59441520102eaa25260000bf/solutions/csharp

---
+++ Kata/FiveWithoutNumbers.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    internal class FiveWithoutNumbers
+    {
+        public static int UnusualFive()
+        {
+            return new bool[] {true,true,true,true,true}.Length;
+        }
+    }
+}
