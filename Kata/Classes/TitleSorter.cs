d99c764afbc4fde85f16544b66f152f20fcf6a94
Auvsexvc <auvsexvc@gmail.com>
2022-08-21 21:46:43 +0200

    https://www.codewars.com/kata/5909cd53e0db4280740000eb/solutions/csharp

---
+++ Kata/Classes/TitleSorter.cs
@@ -0,0 +1,71 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Kata.Classes
+{
+    public class TitleSorter
+    {
+        public List<string> Sort(List<string> unsortedTitles)
+        {
+            foreach (var item in unsortedTitles)
+            {
+                Console.Write(item+", ");
+            }
+            Console.WriteLine();
+            var y = unsortedTitles.Select(s => s.Split(" ")).ToList();
+            var x= unsortedTitles != null
+                ? unsortedTitles
+                    .OrderBy(x => Regex.IsMatch(x, @"^((A|An|The)\b)") && x.Split(" ").Length > 1 && x.Split(" ").All(x=>x!="")
+                        ? String.Join(" ", x.Split(" ").Skip(1)) + (string.Concat(", ", x.Split(" ")[0]))
+                        : x.TrimEnd())
+                    .ToList()
+                : null;
+            return x;
+        }
+
+        public List<string> Sort2(List<string> unsortedTitles)
+        {
+            return unsortedTitles?.OrderBy(SortKey).ToList();
+        }
+
+        static readonly Regex SortKeyRe = new Regex(@"\A(The|An?)\s+(.+)\z");
+        static string SortKey(string s) => SortKeyRe.Replace(s, "$2, $1");
+    }
+
+    [TestFixture]
+    public class TitleSorterTests
+    {
+        [Test]
+        public void SampleBooksSortTest()
+        {
+            List<string> titlesToSort = new List<string>()
+              {
+                "A Petition to Magic",
+                "Heritage of Deceit",
+                "Stingers",
+                "Billy's Zombie",
+                "Heaven and Earth: Paranormal Flash Fiction",
+                "Tales From Virdura",
+              };
+            List<string> sortedList = new TitleSorter().Sort(titlesToSort);
+            Assert.AreEqual(titlesToSort.Count, sortedList.Count);
+            Assert.AreEqual("Billy's Zombie", sortedList[0]);
+            Assert.AreEqual("A Petition to Magic", sortedList[3]);
+        }
+        [Test]
+        public void SampleBooksSortTest2()
+        {
+            List<string> titlesToSort = new List<string>()
+              {
+                "A", "An", "The", "A " , "The " , "An " ,
+              };
+            List<string> sortedList = new TitleSorter().Sort(titlesToSort);
+            Assert.AreEqual(titlesToSort.Count, sortedList.Count);
+            Assert.AreEqual("A", sortedList[0]);
+            //Assert.AreEqual("A Petition to Magic", sortedList[3]);
+        }
+    }
+}
