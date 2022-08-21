98c5d773802bf6afc77d92ed8bf8c3ef054ec348
Auvsexvc <auvsexvc@gmail.com>
2022-08-21 20:39:24 +0200

    https://www.codewars.com/kata/59f38b033640ce9fc700015b/solutions/csharp

---
+++ Kata/Classes/SumOfPrimeIndexedElements.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class SumOfPrimeIndexedElementsTest
+    {
+        [Test]
+        public void ExampleTests()
+        {
+            Assert.AreEqual(0, SumOfPrimeIndexedElements.solve(new int[] { }));
+            Assert.AreEqual(7, SumOfPrimeIndexedElements.solve(new int[] { 1, 2, 3, 4 }));
+            Assert.AreEqual(13, SumOfPrimeIndexedElements.solve(new int[] { 1, 2, 3, 4, 5, 6 }));
+            Assert.AreEqual(47, SumOfPrimeIndexedElements.solve(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 }));
+        }
+    }
+
+    internal class SumOfPrimeIndexedElements
+    {
+        public static int solve(int[] arr)
+        {
+            return arr.Where((_,i) => IsPrime(i)).Sum();
+        }
+
+        private static bool IsPrime(int n)
+        {
+            return n > 1 && Enumerable.Range(2, (int)Math.Sqrt(n) - 1).All(i => n % i != 0);
+        }
+    }
+}
