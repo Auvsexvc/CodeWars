acd27b9ce4aca6e377e3e5e29a94e5a263dbca81
Auvsexvc <auvsexvc@gmail.com>
2022-09-29 10:44:28 +0200

    cleanup

--- Kata/Program.cs
+++ Kata/Program.cs
@@ -1,17 +1,9 @@
-﻿using Kata.Classes;
-using Kata.Classes.Unfinished;
-using System.Collections.Generic;
-using System;
-using System.Linq;
-
-namespace Kata
+﻿namespace Kata
 {
     public static class Kata
     {
         private static void Main(string[] args)
         {
-            if (args == null)
-                throw new ArgumentNullException(nameof(args));
         }
     }
 }
