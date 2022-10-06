b1397998e9915656387738bd357e925ac6502abd
Auvsexvc <auvsexvc@gmail.com>
2022-10-06 17:08:42 +0200

    https://www.codewars.com/kata/5f885fa9f130ea00207c7dc8/solutions/csharp

---
+++ Kata/Classes/LoneliestCharacter.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Kata.Classes
+{
+    [TestFixture]
+    public class LoneliestCharacterSolutionTest
+    {
+        [Test]
+        public void MyTest()
+        {
+            Assert.AreEqual(new char[] { 'b', 'c' }, LoneliestCharacter.Loneliest("a  b  c  de  ").OrderBy(x => x).ToArray());
+            Assert.AreEqual(new char[] { 'g' }, LoneliestCharacter.Loneliest("abc d   ef  g   h i j      "));
+            Assert.AreEqual(new char[] { 'a' }, LoneliestCharacter.Loneliest("a"));
+            Assert.AreEqual(new char[] { 'b' }, LoneliestCharacter.Loneliest("a   b   c"));
+            Assert.AreEqual(new char[] { 'z' }, LoneliestCharacter.Loneliest("  abc  d  z    f gk s "));
+            Assert.AreEqual(new char[] { 'a', 'b', 'c' }, LoneliestCharacter.Loneliest("abc").OrderBy(x => x).ToArray());
+        }
+    }
+
+    internal class LoneliestCharacter
+    {
+        public static char[] Loneliest(string result)
+        {
+            return result
+                .Trim()
+                .Where(c => !char.IsWhiteSpace(c))
+                .GroupBy(c => Regex
+                    .Match(result.Trim(), string.Format(@"([\s]*{0}[\s]*)", c))
+                    .Value
+                    .Count(char.IsWhiteSpace), c => c)
+                .OrderByDescending(g=>g.Key)
+                .Select(g => g.ToArray())
+                .FirstOrDefault();
+        }
+    }
+}
