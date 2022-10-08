37f858f0e3e47218d2d7749cfcdafd45200e7763
Auvsexvc <auvsexvc@gmail.com>
2022-10-08 09:59:19 +0200

    https://www.codewars.com/kata/589b81791491932da40000a3/solutions/csharp

---
+++ Kata/Classes/FindOutARestAfterRm-f.cs
@@ -0,0 +1,52 @@
+﻿using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Kata.Classes
+{
+    internal class FindOutARestAfterRm_f
+    {
+        public class Kata
+        {
+            public static int CountRestAfterRmF(string mask, List<string> files)
+            {
+                return string.IsNullOrEmpty(mask)
+                    ? files.Count
+                    : files.Count(f => !Regex.IsMatch(f,
+                                                      new StringBuilder(mask)
+                                                          .Replace(".", @"\.")
+                                                          .Replace("*", ".*")
+                                                          .Replace("?", ".?")
+                                                          .Insert(0, '^')
+                                                          .Append('$')
+                                                          .ToString()));
+            }
+        }
+
+        [TestFixture]
+        public class KataTests
+        {
+            [Test]
+            public void BasicTest1()
+            {
+                string mask = "?a*";
+
+                List<string> files = new List<string> { "5", "a", "mama.jpg", "ma", "aa", "bb" };
+
+                Assert.AreEqual(2, Kata.CountRestAfterRmF(mask, files));
+            }
+
+            [Test]
+            public void BasicTest2()
+            {
+                string mask = "";
+
+                List<string> files = new List<string> { "fotos", "trash.tar", "june2016", "codewars", "dad", "mom" };
+
+                Assert.AreEqual(6, Kata.CountRestAfterRmF(mask, files));
+            }
+        }
+    }
+}
