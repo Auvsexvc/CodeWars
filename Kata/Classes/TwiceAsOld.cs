b44fe94db4c37fef5752eb70d2453c967aba1574
Paweł Kordaczuk <pawel.kordaczuk@macrix.pl>
2022-10-23 13:27:43 +0200

    https://www.codewars.com/kata/5b853229cfde412a470000d0/solutions/csharp

---
+++ Kata/Classes/TwiceAsOld.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Kata.Classes
+{
+  public class TwiceAsOld
+  {
+    public class TwiceAsOldSolution
+    {
+      public static int TwiceAsOld(int dadYears, int sonYears)
+      {
+        return Math.Abs(dadYears - 2 * sonYears);
+      }
+    }
+
+    [TestFixture]
+    public class TwiceAsOldTest
+    {
+      [Test]
+      public void SampleTests()
+      {
+        Assert.AreEqual(30, TwiceAsOldSolution.TwiceAsOld(30, 0));
+        Assert.AreEqual(16, TwiceAsOldSolution.TwiceAsOld(30, 7));
+        Assert.AreEqual(15, TwiceAsOldSolution.TwiceAsOld(45, 30));
+      }
+    }
+  }
+}
