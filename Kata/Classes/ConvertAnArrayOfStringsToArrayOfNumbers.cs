401c80d62323cc1f322aa63d9eb38bce7bdcf283
Paweł Kordaczuk <pawel.kordaczuk@macrix.pl>
2022-10-30 00:22:05 +0200

    https://www.codewars.com/kata/5783d8f3202c0e486c001d23/solutions/csharp

---
+++ Kata/Classes/ConvertAnArrayOfStringsToArrayOfNumbers.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Kata.Classes
+{
+  internal class ConvertAnArrayOfStringsToArrayOfNumbers
+  {
+    public class stringArrayMethods
+    {
+      public static double[] ToDoubleArray(string[] stringArray)
+      {
+        return stringArray.Select(x => double.Parse(x)).ToArray();
+      }
+    }
+
+    [TestFixture]
+    public class stringArrayMethodsTest
+    {
+      [Test]
+      public void ExampleTest()
+      {
+        Console.WriteLine("Running ToDoubleArray(\"1.1\",\"2.2\",\"3.3\")...");
+        Assert.AreEqual(new double[] { 1.1, 2.2, 3.3 }, stringArrayMethods.ToDoubleArray(new string[] { "1.1", "2.2", "3.3" }));
+      }
+    }
+  }
+}
