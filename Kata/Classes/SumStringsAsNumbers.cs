c0159cd99542c50c6c538cd333bea332f6b8441e
Paweł Kordaczuk <pawel.kordaczuk@macrix.pl>
2022-10-29 15:08:52 +0200

    https://www.codewars.com/kata/5324945e2ece5e1f32000370/solutions/csharp

---
+++ Kata/Classes/SumStringsAsNumbers.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using System.Numerics;
+
+namespace Kata.Classes
+{
+  public class SumStringsAsNumbers
+  {
+    public static class Kata
+    {
+      public static string sumStrings(string a, string b)
+      {
+        return new string[] { a, b }
+          .Select(x => BigInteger.TryParse(x, out BigInteger number) ? number : 0)
+          .Aggregate((x, y) => BigInteger.Add(x, y))
+          .ToString();
+      }
+    }
+
+    [TestFixture]
+    public class CodeWarsTest
+    {
+      [Test]
+      public void Given123And456Returns579()
+      {
+        Assert.AreEqual("579", Kata.sumStrings("123", "456"));
+      }
+
+      [Test]
+      public void GivenEmptyAnd5Returns5()
+      {
+        Assert.AreEqual("5", Kata.sumStrings("", "5"));
+      }
+    }
+  }
+}
