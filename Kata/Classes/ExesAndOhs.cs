d54c857a005f7fe3e673e5d49615b471f5495eda
Paweł Kordaczuk <pawel.kordaczuk@macrix.pl>
2022-10-29 18:17:24 +0200

    https://www.codewars.com/kata/55908aad6620c066bc00002a/solutions/csharp

---
+++ Kata/Classes/ExesAndOhs.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Kata.Classes
+{
+  internal class ExesAndOhs
+  {
+    public static class Kata
+    {
+      public static bool XO(string input)
+      {
+        return input.Count(c => char.ToLower(c) == 'x') == input.Count(c => char.ToLower(c) == 'o');
+      }
+    }
+
+    [TestFixture]
+    public class SolutionTest
+    {
+      [Test]
+      public void ExampleTests()
+      {
+        Assert.AreEqual(true, Kata.XO("xo"));
+        Assert.AreEqual(true, Kata.XO("xxOo"));
+        Assert.AreEqual(false, Kata.XO("xxxm"));
+        Assert.AreEqual(false, Kata.XO("Oo"));
+        Assert.AreEqual(false, Kata.XO("ooom"));
+      }
+    }
+  }
+}
