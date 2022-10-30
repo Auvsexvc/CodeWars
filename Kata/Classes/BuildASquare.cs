8ce55d15264cd610ef69e8bc4137d36f2265a615
Paweł Kordaczuk <pawel.kordaczuk@macrix.pl>
2022-10-30 16:31:12 +0100

    https://www.codewars.com/kata/59a96d71dbe3b06c0200009c/solutions/csharp

---
+++ Kata/Classes/BuildASquare.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System.Text;
+
+namespace Kata.Classes
+{
+  internal class BuildASquare
+  {
+    public static class Kata
+    {
+      public static string GenerateShape(int n)
+      {
+        StringBuilder sb = new();
+        for (int i = 1; i <= n; i++)
+        {
+          for (int j = 1; j <= n; j++)
+          {
+            sb.Append('+');
+          }
+          if (i != n)
+            sb.Append('\n');
+        }
+        return sb.ToString();
+      }
+    }
+
+    [TestFixture]
+    public class SolutionTest
+    {
+      [Test]
+      public void SampleTest()
+      {
+        Assert.AreEqual("", Kata.GenerateShape(0));
+        Assert.AreEqual("+", Kata.GenerateShape(1));
+        Assert.AreEqual("++\n++", Kata.GenerateShape(2));
+        Assert.AreEqual("+++\n+++\n+++", Kata.GenerateShape(3));
+      }
+    }
+  }
+}
