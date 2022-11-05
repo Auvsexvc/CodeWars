2d9d0c106107b25937cb1a72a9e4aff644be5285
Paweł Kordaczuk <pawel.kordaczuk@macrix.pl>
2022-11-05 13:12:14 +0100

    https://www.codewars.com/kata/54c27a33fb7da0db0100040e/solutions/csharp

---
+++ Kata/Classes/YouReASquare.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace Kata.Classes
+{
+  internal class YouReASquare
+  {
+    public class Kata
+    {
+      public static bool IsSquare(int n)
+      {
+        return Math.Sqrt(n) % 1 == 0;
+      }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+      [Test]
+      public static void ShouldWorkForSomeExamples()
+      {
+        Assert.AreEqual(false, Kata.IsSquare(-1), "negative numbers aren't square numbers");
+        Assert.AreEqual(false, Kata.IsSquare(3), "3 isn't a square number");
+        Assert.AreEqual(true, Kata.IsSquare(4), "4 is a square number");
+        Assert.AreEqual(true, Kata.IsSquare(25), "25 is a square number");
+        Assert.AreEqual(false, Kata.IsSquare(26), "26 isn't a square number");
+      }
+    }
+  }
+}
