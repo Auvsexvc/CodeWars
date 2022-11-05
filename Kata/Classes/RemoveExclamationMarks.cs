a3f5d795cb361fb61ec07635be51c821bc80221b
Paweł Kordaczuk <pawel.kordaczuk@macrix.pl>
2022-11-05 12:47:25 +0100

    https://www.codewars.com/kata/57a0885cbb9944e24c00008e/solutions/csharp

---
+++ Kata/Classes/RemoveExclamationMarks.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System.Text.RegularExpressions;
+
+namespace Kata.Classes
+{
+  internal class RemoveExclamationMarks
+  {
+    public class Kata
+    {
+      public static string RemoveExclamationMarks(string s)
+      {
+        return Regex.Replace(s, "[!]", "");
+      }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+      [Test]
+      public static void MyTests()
+      {
+        Assert.AreEqual("", Kata.RemoveExclamationMarks(""), "Input: Empty string");
+        Assert.AreEqual("", Kata.RemoveExclamationMarks("!"), "Input: " + "!");
+        Assert.AreEqual("", Kata.RemoveExclamationMarks("!!"), "Input: " + "!!");
+        Assert.AreEqual("Hi", Kata.RemoveExclamationMarks("Hi!"), "Input: " + "Hi!");
+        Assert.AreEqual("?", Kata.RemoveExclamationMarks("!?!"), "Input: " + "!?!");
+      }
+    }
+  }
+}
