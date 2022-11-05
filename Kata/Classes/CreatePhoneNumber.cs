a0c49c4840fdaade0715d770379adfacabf0d614
Paweł Kordaczuk <pawel.kordaczuk@macrix.pl>
2022-11-05 13:31:07 +0100

    https://www.codewars.com/kata/525f50e3b73515a6db000b83/solutions/csharp

---
+++ Kata/Classes/CreatePhoneNumber.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using System.Text;
+
+namespace Kata.Classes
+{
+  public class CreatePhoneNumber
+  {
+    public class Kata
+    {
+      public static string CreatePhoneNumber(int[] numbers)
+      {
+        return new StringBuilder("(")
+          .AppendJoin("", numbers.Select((v, i) => i == 2
+            ? $"{v}) "
+            : i == 5
+              ? $"{v}-"
+              : v.ToString()))
+          .ToString();
+      }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+      [Test]
+      [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, ExpectedResult = "(123) 456-7890")]
+      [TestCase(new int[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 }, ExpectedResult = "(111) 111-1111")]
+      public static string FixedTest(int[] numbers)
+      {
+        return Kata.CreatePhoneNumber(numbers);
+      }
+    }
+  }
+}
