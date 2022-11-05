1d5d8f6ef991500753f9447774fd0eb9ff535b94
Paweł Kordaczuk <pawel.kordaczuk@macrix.pl>
2022-11-05 13:03:42 +0100

    https://www.codewars.com/kata/57cff961eca260b71900008f/solutions/csharp

---
+++ Kata/Classes/IsThereAVowelInThere.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Kata.Classes
+{
+  internal class IsThereAVowelInThere
+  {
+    public class Kata
+    {
+      public static object[] IsVow(object[] a)
+      {
+        return a
+          .Select(numb => new char[] { 'a', 'e', 'i', 'o', 'u' }.Contains(Convert.ToChar(numb))
+            ? Convert.ToChar(numb).ToString()
+            : numb)
+          .ToArray();
+      }
+    }
+
+    [TestFixture]
+    public class KataTests
+    {
+      [Test]
+      public void BasicTests()
+      {
+        Assert.AreEqual(new object[] { 118, "u", 120, 121, "u", 98, 122, "a", 120, 106, 104, 116, 113, 114, 113, 120, 106 }, Kata.IsVow(new object[] { 118, 117, 120, 121, 117, 98, 122, 97, 120, 106, 104, 116, 113, 114, 113, 120, 106 }));
+        Assert.AreEqual(new object[] { "e", 121, 110, 113, 113, 103, 121, 121, "e", 107, 103 }, Kata.IsVow(new object[] { 101, 121, 110, 113, 113, 103, 121, 121, 101, 107, 103 }));
+      }
+    }
+  }
+}
