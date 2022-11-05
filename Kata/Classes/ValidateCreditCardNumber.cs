16beaeb502d3fd400eed9e3694a161a58eb0b906
Paweł Kordaczuk <pawel.kordaczuk@macrix.pl>
2022-11-05 18:51:06 +0100

    https://www.codewars.com/kata/5418a1dd6d8216e18a0012b2/solutions/csharp

---
+++ Kata/Classes/ValidateCreditCardNumber.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using System.Linq;
+
+namespace Kata.Classes
+{
+  public class ValidateCreditCardNumber
+  {
+    public class Solution
+    {
+      public static bool Validate(string n)
+      {
+        return n
+          .Where(x => char.IsDigit(x))
+          .Reverse()
+          .Select(x => int.Parse(x.ToString()))
+          .Select((v, i) => i % 2 == 0
+            ? v
+            : v * 2 > 9
+              ? (v * 2) - 9
+              : v * 2)
+          .Sum() % 10 == 0;
+      }
+    }
+
+    [TestFixture]
+    public class SampleTests
+    {
+      [Test]
+      public void TestCases()
+      {
+        var _ = new Solution();
+        Assert.AreEqual(false, Solution.Validate("477 073 360"));
+        Assert.AreEqual(true, Solution.Validate("5422 0148 5514"));
+        Assert.AreEqual(true, Solution.Validate("8314 7046 0245"));
+        Assert.AreEqual(false, Solution.Validate("6654 6310 43044"));
+        Assert.AreEqual(true, Solution.Validate("0768 2757 5685 6340"));
+        Assert.AreEqual(false, Solution.Validate("7164 6207 74042"));
+        Assert.AreEqual(true, Solution.Validate("8383 7332 3570 8514"));
+        Assert.AreEqual(true, Solution.Validate("481 135"));
+        Assert.AreEqual(true, Solution.Validate("355 032 5363"));
+      }
+    }
+  }
+}
