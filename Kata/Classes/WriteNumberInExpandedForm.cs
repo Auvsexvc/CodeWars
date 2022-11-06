1e52db952dc674851dfa0f3053f230437bac3e6f
Paweł Kordaczuk <pawel.kordaczuk@macrix.pl>
2022-11-06 21:53:10 +0100

    https://www.codewars.com/kata/5842df8ccbd22792a4000245/solutions/csharp

---
+++ Kata/Classes/WriteNumberInExpandedForm.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace Kata.Classes
+{
+  public class WriteNumberInExpandedForm
+  {
+    public static class Kata
+    {
+      public static string ExpandedForm(long num)
+      {
+        return string.Join(
+          " + ",
+          num
+            .ToString()
+            .Select((d, i) => (int.Parse(d.ToString()) * Math.Pow(10, num.ToString().Length - i - 1)).ToString())
+            .Where(s => s != "0"));
+      }
+    }
+
+    [TestFixture]
+    public class SolutionTest
+    {
+      [Test]
+      public void BasicTests()
+      {
+        Assert.That(Kata.ExpandedForm(12), Is.EqualTo("10 + 2"));
+        Assert.That(Kata.ExpandedForm(42), Is.EqualTo("40 + 2"));
+        Assert.That(Kata.ExpandedForm(70304), Is.EqualTo("70000 + 300 + 4"));
+      }
+    }
+  }
+}
