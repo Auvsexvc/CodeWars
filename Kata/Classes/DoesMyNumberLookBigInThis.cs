2b935496a6fd13a51e2cd3ef2809b02e82641f02
Paweł Kordaczuk <pawel.kordaczuk@macrix.pl>
2022-11-06 17:31:24 +0100

    https://www.codewars.com/kata/5287e858c6b5a9678200083c/solutions/csharp

---
+++ Kata/Classes/DoesMyNumberLookBigInThis.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Kata.Classes
+{
+  internal class DoesMyNumberLookBigInThis
+  {
+    public class Kata
+    {
+      public static bool Narcissistic(int value)
+      {
+        return value
+          .ToString()
+          .Select(d => Math.Pow(int.Parse(d.ToString()), value.ToString().Length))
+          .Sum() == value;
+      }
+    }
+
+    [TestFixture]
+    public class Sample_Test
+    {
+      private static IEnumerable<TestCaseData> testCases
+      {
+        get
+        {
+          yield return new TestCaseData(1)
+                          .Returns(true)
+                          .SetDescription("1 is narcissitic");
+          yield return new TestCaseData(371)
+                          .Returns(true)
+                          .SetDescription("371 is narcissitic");
+        }
+      }
+
+      [Test, TestCaseSource("testCases")]
+      public bool Test(int n) => Kata.Narcissistic(n);
+    }
+  }
+}
